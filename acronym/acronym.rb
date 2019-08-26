fd6bbe00af48d07de807c7798df431b649297b51
luminousbeam <jocelyn.jeriah@gmail.com>
2019-08-26 15:40:38 -0400

    refactor acronym solution

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -1,7 +1,6 @@
 class Acronym
   def self.abbreviate(string)
-    string_arr = string.scan(/\w+/)
-    words = string_arr.map { |first_letter| first_letter[0]}
-    words.join.upcase
+    string_arr = string.scan(/\b\w/)
+    string_arr.join.upcase
   end
 end
