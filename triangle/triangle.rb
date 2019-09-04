f280b6d80be4455d903376a1b579c616a71bc8f8
luminousbeam <jocelyn.jeriah@gmail.com>
2019-09-04 10:01:22 -0400

    add solution for equilateral triangle

--- triangle/triangle.rb
+++ triangle/triangle.rb
@@ -1,12 +1,22 @@
 require 'minitest/pride'
 
 class Triangle
+  attr_reader :side_lengths
+
   def initialize(side_lengths)
     @side_lengths = side_lengths
   end
 
-  def equilateral
-    side_lengths.map { |side|  }
+  def illegal?
+    side_lengths.any? { |side| side <= 0 }
+  end
+
+  def equilateral?
+    if illegal?
+      false
+    else
+      side_lengths.uniq.size == 1
+    end
   end
 
   def isosceles
